e06c2a4171fe46a64aaa2e6b3a680d2d7cf295d0
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-04-03 09:41:10 +0000

    create the draft files

--- component/tile/translator_tile.py
+++ component/tile/translator_tile.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from shutil import copyfile
 
 from sepal_ui import sepalwidgets as sw 
 from traitlets import Unicode
@@ -6,15 +7,17 @@ from traitlets import Unicode
 
 class TranslatorTile(sw.Tile):
     
-    folder = Unicode('').tag(sync=True)
-    
     def __init__(self, user_folder):
         
         # gather the info 
-        self.folder = user_folder
+        self.folder = Path(user_folder).expanduser()
         
         # set the available locales 
         self.locales = self._get_locales()
+        
+        # initialized the draft files (if needed)
+        self._init_drafts()
+        
         
         # create the actual tile
         super().__init__(
@@ -28,9 +31,7 @@ class TranslatorTile(sw.Tile):
     def _get_locales(self):
         """read the folder searching for json files and get the available languages of the app"""
         
-        message_dir = Path(self.folder)
-        
-        locales = [f.stem for f in message_dir.glob('*.json')]
+        locales = [f.stem for f in self.folder.glob('*.json')]
         
         # check that at least english exist
         if not ('en' in locales):
@@ -41,6 +42,25 @@ class TranslatorTile(sw.Tile):
             raise Exception("You don't have any target language")
             
         return locales
+    
+    def _init_drafts(self):
+        """init the draft files if needed, we'll use the already existing one if they exist"""
+        
+        # we don't create drafts fo en.json as it is the reference
+        locales = [l for l in self.locales if l != 'en']
+        
+        # create a draft file if needed
+        for l in locales:
+            valid = self.folder.joinpath(f'{l}.json')
+            draft = valid.with_suffix(valid.suffix + '.draft')
+            
+            if not draft.is_file():
+                copyfile(valid, draft)
+         
+        
+        return self
+        
+        
             
         
         
